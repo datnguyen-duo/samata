5a8b33ce50aa61fd3af6fb7bec86a8ad6a3d1b64
Dat Nguyen <Dat_T_Nguyen@live.com>
2020-09-08 20:44:58 -0400

    resize function

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 gsap.registerPlugin(ScrollToPlugin);
 
-// $(window).bind("resize", function (e) {
-//   if (window.RT) clearTimeout(window.RT);
-//   window.RT = setTimeout(function () {
-//     this.location.reload(false); /* false to get page from cache */
-//   }, 100);
-// });
+$(window).bind("resize", function (e) {
+  if (window.RT) clearTimeout(window.RT);
+  window.RT = setTimeout(function () {
+    this.location.reload(false); /* false to get page from cache */
+  }, 100);
+});
 
 function bannerLanding() {
   let mainImg = $("#banner .img-wrapper");
