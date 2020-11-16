2fff95f9c0ce8fe15db352ac1f3e3bf388aec2fc
Dat Nguyen <Dat_T_Nguyen@live.com>
2020-11-16 14:51:40 -0500

    forms

--- js/contact.js
+++ js/contact.js
@@ -73,3 +73,71 @@ $(function () {
     $(this).closest("nav").toggleClass("open");
   });
 });
+
+$(function () {
+  $(".formkit-form").on("submit", function (event) {
+    var origin = window.location.origin;
+    var url = origin + "/mail/mail.php";
+    var fname = "";
+    var lname = "";
+    var email = "";
+    var company = "";
+    var message = "";
+    var location_therapy = "";
+    var licence_type = "";
+    var licence_number = "";
+    var link_website = "";
+    var practice = "";
+    var action = "";
+    fname = $(this).find('input[name="fields[first_name]"]').val();
+    lname = $(this).find('input[name="fields[last_name]"]').val();
+    company = $(this).find('input[name="fields[company]"]').val();
+    message = $(this).find('input[name="fields[message]"]').val();
+    email = $(this).find('input[name="email_address"]').val();
+    if ($(this).attr("data-sv-form") == "1653865") {
+      action = "contact_for_therapists";
+    } else if ($(this).attr("data-sv-form") == "1653463") {
+      action = "contact_for_employers";
+    } else if ($(this).attr("data-sv-form") == "1653624") {
+      action = "contact_for_therapists";
+    } else if ($(this).attr("data-sv-form") == "1653633") {
+      action = "contact_for_individuals";
+    } else if ($(this).attr("data-sv-form") == "1653666") {
+      action = "footer_home_form";
+    } else if ($(this).attr("data-sv-form") == "1163245") {
+      action = "footer_home_form";
+    } else if ($(this).attr("data-sv-form") == "1657833") {
+      action = "contact_for_apply_now";
+      lname = $(this).find('input[aria-label="Last Name"]').val();
+      location_therapy = $(this)
+        .find('input[name="fields[location_of_therapy_practice]"]')
+        .val();
+      licence_type = $(this).find('input[name="fields[license_type]"]').val();
+      licence_number = $(this).find('input[aria-label="License Number"]').val();
+      link_website = $(this).find('input[aria-label="Link to Website"]').val();
+      practice = $(this)
+        .find('input[aria-label="Tell us a bit about your practice"]')
+        .val();
+    }
+    $.ajax({
+      url: url,
+      data: {
+        action: action,
+        email: email,
+        fname: fname,
+        lname: lname,
+        company: company,
+        message: message,
+        location_therapy: location_therapy,
+        licence_type: licence_type,
+        licence_number: licence_number,
+        link_website: link_website,
+        practice: practice,
+      },
+      type: "POST",
+      success: function (data) {
+        console.log(data);
+      },
+    });
+  });
+});
